d37d229dddd26567605b890677c9fa21cda7e603
Phuc H Duong <phuchduong@datasciencedojo.com>
2015-04-29 19:25:58 -0700

    more legacy code added

---
+++ JS/TempyUtil.js
@@ -0,0 +1,9 @@
+
+// Returns a random integer between min and max (inclusive).
+function randomIntRange(min, max) {
+    if (max == null) {
+        max = min;
+        min = 0;
+    }
+    return min + Math.floor(Math.random() * (max - min + 1));
+};
