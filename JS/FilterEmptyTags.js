3cd1e3419426cbeb8027f062b8dcbb93c1a05c4c
Phuc H Duong <phuchduong@datasciencedojo.com>
2015-07-29 04:54:15 -0700

    added mongodb ref

--- JS/FilterEmptyTags.js
+++
@@ -1,7 +0,0 @@
-function filterEmptyTag(tagSelector){
-	$(tagSelector).each(function() {
-	    var $this = $(this);
-	    if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-	        $this.remove();
-	});
-};
