b61629c807817626aef8731a4c372281a0fa571e
Phuc H Duong <phuchduong@datasciencedojo.com>
2015-08-27 09:25:38 +0800

    updated schema validations

--- Python/AzureMLClient.py
+++ Python/AzureMLClient.py
@@ -7,7 +7,7 @@ class AzureMLClient:
 		self.apiKey = ''
 		self.postURL = ''
 		self.columnNames = []
-		self.requestBody = []
+		self.requestBody = [ [],[] ]
 		self.schemaBase = {
 				"Inputs":{  
 					"input1":{  
@@ -24,6 +24,7 @@ class AzureMLClient:
 			}
 
 
+	# executes a request to azure ml, if payload is valid
 	def makePrediction(self):
 		schemaReport = validSchema()
 		if(schemaReport == 'valid'):
@@ -33,33 +34,60 @@ class AzureMLClient:
 			return schemaReport
 
 
+	# validates payload. Checks all the fields before building a
+	# payload. Returns 'valid' if valid, and returns an error report
+	# if not valid
 	def validSchema():
 		messageList = []
-		if(if not self.apiKey):
+		if( len(self.apiKey) == 0 ):
 			messageList.append("API key is missing.")
-		elif(if not self.postURL):
+		elif( len(self.apiKey) != 88 ):
+			# silently logs warning. Incase AzureML changes their API
+			# keys in the future, this function won't break.
+			print "Warning: the API key is not exactly 88 characters and may be invalid."
+		
+		if( len(self.postURL) == 0 ):
 			messageList.append("Post URL for webservice is missing.")
-		elif(if not self.columnNames):
+		elif( len(self.apiKey) != 166 || len(self.apiKey) != 135 ):
+			# silently logs warning. Incase AzureML changes their API
+			# postURLs in the future, this function won't break.
+			# newer post URLs have 166 characters. Legacy post URLs from beta
+			# had 135 characters.
+			print "Warning: the post URL is not exactly 166 or 135 characters and may be invalid."
+		
+		if( len(self.columnNames) == 0 ):
 			messageList.append("Client requires column names.")
-		elif(if not self.requestBody):
-			messageList.append("Client requires request body.")
+		
+		try:
+			if( type(self.requestBody[0]) != list):
+				# if nested list array, it is a valid schema
+				messageList.append("Client requires the request body to be a nested list of lists.")
+				if( len(self.requestBody[0]) < 1):
+					# checks if the first nested list is empty
+					messageList.append("Empty inputs within the request body.")
+		except Exception:
+			# bad variable typing. Int, empty string, empty list etc.
+			messageList.append("Client requires a valid request body.")
+			pass
 
 		if(if not messageList):
-			messageReport = ''.join(messageList)
+			messageReport = ' '.join(messageList)
 			print messageReport # Logs error server side.
 			return messageReport
-		else
+		else:
 			return 'valid'
 
 
 	# Builds the request body from the base, column names and values
+	# requestBody is a nested array: [ [],[],[] ]
 	def buildSchema(self):
 		schemaJSON = JSON.dumps(self.schemaBase)
 		schemaJSON['Inputs']['input1']['ColumnNames'] = self.columnNames
 		schemaJSON['Inputs']['input1']['Values'] = self.requestBody
 		return schemaJSON
 
-
+		
+	# sends request to AzureML webservice, returns a response.
 	def sendResponse(self):
 		schemaJSON = buildSchema()
 		schemaStr = str(schemaJSON)
